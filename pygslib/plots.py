50979ce76b1b2d038e112bb45b80b98f49f73846
Roberto Rolo <robertorolo@gmail.com>
2019-07-15 19:17:46 -0300

    grid working

--- pygslib/plots.py
+++ pygslib/plots.py
@@ -257,19 +257,16 @@ def cell_declus_sum(cell_size, mean, title='Cell declus summary', pointsize=8, f
 
     return pyo.iplot(fig)
 
-def pixelplot(grid_dic, variable, categorical=False, points=None, gap=1, title='', x_axis='Easting (m)', y_axis='Northing (m)', colorscale='Jet', colorbartitle='', figsize=(700,700)):
+def pixelplot(grid_dic, variable, categorical=False, points=None, gap=0, title='', x_axis='Easting (m)', y_axis='Northing (m)', colorscale='Jet', colorbartitle='', figsize=(700,700)):
 
     traces = []
 
-    x = [(grid_dic['ox']+i*grid_dic['sx']) for i in range(grid_dic['nx'])]
-    y = [(grid_dic['oy']+j*grid_dic['sy']) for j in range(grid_dic['ny'])]
-
-    print(x)
-    print(y)
+    x = np.array([(grid_dic['ox']+i*grid_dic['sx']) for i in range(grid_dic['nx'])])
+    y = np.array([(grid_dic['oy']+j*grid_dic['sy']) for j in range(grid_dic['ny'])])
 
     trace = {
     'type':'heatmap',
-    'z':variable.reshape(grid_dic['nx'], grid_dic['ny']),
+    'z':variable.reshape(grid_dic['ny'], grid_dic['nx']),
     'x':x,
     'y':y,
     'colorscale':colorscale,
--- pygslib/utils.py
+++ pygslib/utils.py
@@ -7,16 +7,16 @@ def autogrid(x, y, z, sx, sy, sz, buffer=0):
 		oz = 0
 		max_z = 0
 	else:
-		oz = min(z) - buffer
+		oz = min(z) - buffer + sz/2
 		max_z = max(z) + buffer
-		nz = math.ceil((max_z - oz)/sz) #+ 1
+		nz = math.ceil((max_z - oz)/sz)
 
-	ox = min(x) - buffer
-	oy = min(y) - buffer
+	ox = min(x) - buffer + sx/2
+	oy = min(y) - buffer + sy/2
 	max_x = max(x) + buffer
 	max_y = max(y) + buffer
-	nx = math.ceil((max_x - ox)/sx) #+ 1
-	ny = math.ceil((max_y - oy)/sy) #+ 1
+	nx = math.ceil((max_x - ox)/(sx))
+	ny = math.ceil((max_y - oy)/(sy))
 	
 
 	return {'ox':ox,'oy':oy,'oz':oz,'sx':sx,'sy':sy,'sz':sz,'nx':nx,'ny':ny,'nz':nz}
