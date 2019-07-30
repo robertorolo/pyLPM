e14b615fc685e77978c63192aad7ad6854c4791a
Roberto Rolo <robertorolo@gmail.com>
2019-07-29 22:20:25 -0300

    some tweaks to make pip package work

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'pyLPM',         # How you named your package folder (MyLib)
   packages = ['pyLPM'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Educational geostatistical library based on GSLib algorithms',   # Give a short description about your library
   author = 'LPM - UFRGS',                   # Type in your name
   author_email = 'robertorolo@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/robertorolo/pyLPM',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/robertorolo/pyLPM/archive/alpha.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/robertorolo/pyLPM/archive/1.1.tar.gz',    # I explain this later on
   keywords = ['geostatistics', 'kriging', 'variogram'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -18,17 +18,9 @@ setup(
           'plotly',
           'pandas',
           'IPython',
-          'os',
-          'sys',
           'ipywidgets',
-          'subprocess',
-          'itertools',
-          'math',
           'matplotlib',
           'sklearn',
-          'mpl_toolkits',
-          'warnings',
-          'time'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
