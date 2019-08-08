e165b04d6fff00f1ef6ad839d0e71377e36a5a89
robertorolo <rolo@ar2tech.com>
2019-08-08 12:10:13 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pyLPM',         # How you named your package folder (MyLib)
   packages = ['pyLPM'],   # Chose the same as "name"
-  version = '1.1.0',      # Start with a small number and increase it with every change you make
+  version = '1.3.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Educational geostatistical library based on GSLib algorithms',   # Give a short description about your library
   author = 'LPM - UFRGS',                   # Type in your name
@@ -13,23 +13,24 @@ setup(
   keywords = ['geostatistics', 'kriging', 'variogram'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
-          'scipy',
+          'scipy==1.3.0',
           'numba',
-          'plotly',
+          'plotly==4.0.0',
           'pandas',
           'IPython',
-          'ipywidgets',
+          'ipywidgets==7.5.0',
           'matplotlib',
-          'sklearn',
+          #'sklearn',
+          'scikit-learn==0.21.2'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Education',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    #'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    #'Programming Language :: Python :: 3.4',
+    #'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7'
   ],
